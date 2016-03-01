b5b12285bdf8b5da4d87d2b00c60f47ca99a1060
n.marushkai <unlimited54321@mail.ru>
2016-03-01 22:49:13 +0300

    My first spring project

---
+++ 02. Spring MVC basics/src/main/java/ru/kpfu/itis/controller/IndexController.java
@@ -0,0 +1,32 @@
+package ru.kpfu.itis.controller;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestParam;
+
+@Controller
+public class IndexController {
+
+
+    @RequestMapping("/")
+    public String showIndexPage() {
+        return "index";
+    }
+
+    @RequestMapping("/test")
+    public String showAnotherPage(@RequestParam(value = "result") String result, Model model) {
+        model.addAttribute("text", "Какой-то текст");
+        model.addAttribute("result", result);
+        return "another";
+    }
+
+    @RequestMapping("/test/{username}")
+    public String showUsername(@PathVariable("username") String username, Model model) {
+        model.addAttribute("username", username);
+        return "usernameshow";
+    }
+}
+
+
